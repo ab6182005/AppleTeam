dd1d8627083e126cd53069e8f5418b9d8f9e590d
Baek-sohyeon <55051191+Baek-sohyeon@users.noreply.github.com>
2020-10-06 15:40:15 +0900

    eraser 추가

---
+++ animal_jump(연습용)/Assets/Scripts/EraserMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EraserMovement : MonoBehaviour
+{
+
+    GameObject Player;
+    PlayerMovement playermovement;
+    public float speed=0.9f;
+    public Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        playermovement = Player.GetComponent<PlayerMovement>();
+
+
+        if (playermovement.isGround == true)
+        {
+            Invoke("Update", 30);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Debug.Log("5초 지남");
+        Vector3 player = Player.transform.position;
+        transform.position = Vector3.Lerp(transform.position, player, Time.deltaTime * speed);
+
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Collider other = collision.collider;
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("GameOver");
+        }
+
+    }
+}
